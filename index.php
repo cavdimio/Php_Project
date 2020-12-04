a9006f01f606be6e00066ecad4664f4df44a7d0d
cavdimio <cs.avdimiotis@gmail.com>
2020-12-04 15:06:56 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <!-- <head>
+    <!-- <meta charset="utf-8">
+    <title></title>
+  </head> -->
+  <body>
+
+<?php
+echo "Hello World!";
+echo "Hello Konstantina";
+echo "Hello Christos";
+ ?>
+
+  </body>
+</html>
